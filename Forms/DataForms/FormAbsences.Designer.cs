df3342948b01e26b7400aab86b3d42b9297ebd4a
mikulus8922 <mikulus8922@gmail.com>
2022-01-08 15:57:03 +0100

    abs

--- Forms/DataForms/FormAbsences.Designer.cs
+++ Forms/DataForms/FormAbsences.Designer.cs
@@ -29,12 +29,135 @@ namespace School.Forms.DataForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.comboBoxStudents = new System.Windows.Forms.ComboBox();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.textBoxGrade = new System.Windows.Forms.TextBox();
+            this.textBoxWeight = new System.Windows.Forms.TextBox();
+            this.textBoxType = new System.Windows.Forms.TextBox();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 91);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(758, 424);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_CellMouseClick);
+            // 
+            // comboBoxStudents
+            // 
+            this.comboBoxStudents.FormattingEnabled = true;
+            this.comboBoxStudents.Location = new System.Drawing.Point(18, 50);
+            this.comboBoxStudents.Name = "comboBoxStudents";
+            this.comboBoxStudents.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxStudents.TabIndex = 2;
+            this.comboBoxStudents.SelectedIndexChanged += new System.EventHandler(this.comboBoxStudents_SelectedIndexChanged);
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Enabled = false;
+            this.buttonModify.Location = new System.Drawing.Point(18, 651);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(75, 23);
+            this.buttonModify.TabIndex = 4;
+            this.buttonModify.Text = "Modify";
+            this.buttonModify.UseVisualStyleBackColor = true;
+            this.buttonModify.Click += new System.EventHandler(this.buttonModify_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Enabled = false;
+            this.buttonDelete.Location = new System.Drawing.Point(18, 680);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // textBoxGrade
+            // 
+            this.textBoxGrade.Location = new System.Drawing.Point(18, 533);
+            this.textBoxGrade.Name = "textBoxGrade";
+            this.textBoxGrade.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGrade.TabIndex = 7;
+            this.textBoxGrade.Text = "Grade";
+            // 
+            // textBoxWeight
+            // 
+            this.textBoxWeight.Location = new System.Drawing.Point(124, 533);
+            this.textBoxWeight.Name = "textBoxWeight";
+            this.textBoxWeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWeight.TabIndex = 8;
+            this.textBoxWeight.Text = "Weight";
+            // 
+            // textBoxType
+            // 
+            this.textBoxType.Location = new System.Drawing.Point(230, 533);
+            this.textBoxType.Name = "textBoxType";
+            this.textBoxType.Size = new System.Drawing.Size(100, 20);
+            this.textBoxType.TabIndex = 9;
+            this.textBoxType.Text = "Type";
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(336, 533);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(434, 20);
+            this.textBoxDescription.TabIndex = 10;
+            this.textBoxDescription.Text = "Description";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(695, 680);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 12;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // FormAbsences
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormAbsences";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(58)))));
+            this.ClientSize = new System.Drawing.Size(782, 729);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.textBoxType);
+            this.Controls.Add(this.textBoxWeight);
+            this.Controls.Add(this.textBoxGrade);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.comboBoxStudents);
+            this.Controls.Add(this.dataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormAbsences";
+            this.Text = "FormGrades";
+            this.Load += new System.EventHandler(this.FormGrades_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.ComboBox comboBoxStudents;
+        private System.Windows.Forms.Button buttonModify;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.TextBox textBoxGrade;
+        private System.Windows.Forms.TextBox textBoxWeight;
+        private System.Windows.Forms.TextBox textBoxType;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Button buttonAdd;
     }
 }
--- Forms/DataForms/FormAbsences.cs
+++ Forms/DataForms/FormAbsences.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using School.Logics;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,116 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using School.Data;
+using School.Forms.DataForms.AddForms;
 
 namespace School.Forms.DataForms
 {
     public partial class FormAbsences : Form
     {
+
+        DataTable grades;
+        DataTable classes;
+        DataTable students;
+        DataTable lessons;
+
+        GradesLogic gradesLogic = new GradesLogic();
+        ClassesLogic classesLogic = new ClassesLogic();
+        StudentsLogic studentsLogic = new StudentsLogic();
+        LessonsLogic lessonsLogic = new LessonsLogic();
+
+
+        int selectedIndex;
+
         public FormAbsences()
         {
             InitializeComponent();
+        }
+
+        private void FormGrades_Load(object sender, EventArgs e)
+        {
+            lessons = lessonsLogic.ValidateGetTeacherLessons(UserData.teacherID);
+
+            for (int i = 0; i < classes.Rows.Count; i++)
+            {
+                comboBoxStudents.Items.Add(classes.Rows[i].Field<string>("Name"));
+            }
+
+            grades = gradesLogic.ValidateGetTeacherGrades(UserData.teacherID);
+            dataGridView.DataSource = grades;
+        }
+
+        private void comboBoxStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            selectedIndex = comboBoxStudents.SelectedIndex;
+            int classID = classes.Rows[selectedIndex].Field<int>("ClassID");
+            students = studentsLogic.ValidateGetStudents(classID);
+
+            grades = gradesLogic.ValidateGetClassTeacherGrades(classID, UserData.teacherID);
+            dataGridView.DataSource = grades;
+
+        }
+
+
+        private void dataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            selectedIndex = dataGridView.CurrentCell.RowIndex;
+            DataGridViewRow row = dataGridView.Rows[selectedIndex];
+            textBoxGrade.Text = row.Cells[2].Value.ToString();
+            textBoxWeight.Text = row.Cells[3].Value.ToString();
+            textBoxType.Text = row.Cells[5].Value.ToString();
+            textBoxDescription.Text = row.Cells[7].Value.ToString();
+
+            Console.WriteLine(selectedIndex);
+
+            buttonModify.Enabled = true;
+            buttonDelete.Enabled = true;
+        }
+
+        private void buttonModify_Click(object sender, EventArgs e)
+        {
+            int gradeID = grades.Rows[selectedIndex].Field<int>("GradeID");
+
+
+            int grade = int.Parse(textBoxGrade.Text);
+            int weight = int.Parse(textBoxWeight.Text);
+            string type = textBoxType.Text;
+            string description = textBoxDescription.Text;
+
+
+            bool message = gradesLogic.ValidateEditGrade(gradeID, grade, weight, type, description);
+
+            Console.WriteLine(message);
+
+            grades = gradesLogic.ValidateGetTeacherGrades(UserData.teacherID);
+            dataGridView.DataSource = grades;
+
+
+            buttonModify.Enabled = false;
+            buttonDelete.Enabled = false;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int gradeID = grades.Rows[selectedIndex].Field<int>("GradeID");
+
+            bool message = gradesLogic.ValidateRemoveGrade(gradeID);
+
+            Console.WriteLine(message);
+
+            grades = gradesLogic.ValidateGetTeacherGrades(UserData.teacherID);
+            dataGridView.DataSource = grades;
+
+            buttonModify.Enabled = false;
+            buttonDelete.Enabled = false;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            FormAddGrade formAddGrade = new FormAddGrade();
+            formAddGrade.Show();
+
         }
     }
 }
